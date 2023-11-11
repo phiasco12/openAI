5af32457dbdd841456ebf0af89ba7551076234f5
phiasco12 <50844518+phiasco12@users.noreply.github.com>
2023-11-11 17:53:11 +0000

    Update OpenAITranslationPlugin.java

--- src/android/OpenAITranslationPlugin.java
+++ src/android/OpenAITranslationPlugin.java
@@ -6,54 +6,52 @@ import org.json.JSONArray;
 import org.json.JSONException;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLEncoder;
 
 public class OpenAITranslationPlugin extends CordovaPlugin {
-    private static final String OPENAI_API_KEY = "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"; // Replace with your OpenAI API key
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("translate")) {
-            String text = args.getString(0);
-            String targetLanguage = args.getString(1);
-
-            try {
-                String translatedText = translateText(text, targetLanguage);
-                callbackContext.success(translatedText);
-            } catch (IOException e) {
-                callbackContext.error("Translation failed: " + e.getMessage());
-            }
+        if ("translate".equals(action)) {
+            String textToTranslate = args.getString(0);
+            String translatedText = translateText(textToTranslate);
+            callbackContext.success(translatedText);
             return true;
         }
         return false;
     }
 
-    private String translateText(String text, String targetLanguage) throws IOException {
-        String openaiEndpoint = "https://api.openai.com/v1/engines/davinci-codex/completions";
-        String openaiApiKey = OPENAI_API_KEY;
-        String prompt = text + "\n\nTranslate the above text to " + targetLanguage + ":";
+    private String translateText(String text) {
+        try {
+            // Replace "YOUR_OPENAI_API_ENDPOINT" with the actual OpenAI API endpoint
+            String baseUrl = "https://api.openai.com/v1/translate";
+            String apiKey = "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";
+            String apiUrl = baseUrl + "?text=" + text;
 
-        URL url = new URL(openaiEndpoint);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestMethod("POST");
-        connection.setRequestProperty("Authorization", "Bearer " + openaiApiKey);
-        connection.setDoOutput(true);
+            URL url = new URL(apiUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
-        String postData = "prompt=" + URLEncoder.encode(prompt, "UTF-8");
-        connection.getOutputStream().write(postData.getBytes("UTF-8"));
+            // Set the authorization header with the API key
+            connection.setRequestProperty("Authorization", "Bearer " + apiKey);
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        StringBuilder response = new StringBuilder();
-        String line;
-        while ((line = reader.readLine()) != null) {
-            response.append(line);
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String inputLine;
+                StringBuilder content = new StringBuilder();
+                while ((inputLine = in.readLine()) != null) {
+                    content.append(inputLine);
+                }
+                in.close();
+                return content.toString();
+            } else {
+                // Handle error cases
+                return "Error: " + responseCode;
+            }
+        } catch (Exception e) {
+            return "Exception: " + e.getMessage();
         }
-        reader.close();
-
-        return response.toString();
     }
 }
